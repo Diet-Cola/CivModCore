1e5d0071b2d6ff1a92e4efd6f3fa8b68912b9762
Alexander <protonull@protonmail.com>
2021-06-22 08:34:01 +0100

    Update CivLogger to allow for explicit plugin root

--- src/main/java/vg/civcraft/mc/civmodcore/utilities/CivLogger.java
+++ src/main/java/vg/civcraft/mc/civmodcore/utilities/CivLogger.java
@@ -7,6 +7,7 @@ import java.util.logging.Logger;
 import javax.annotation.Nonnull;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.PluginClassLoader;
 
@@ -36,16 +37,35 @@ public final class CivLogger extends Logger {
 	 * @return Returns a new civ logger.
 	 */
 	public static CivLogger getLogger(@Nonnull final Class<?> clazz) {
-		final ClassLoader classLoader = clazz.getClassLoader();
-		if (classLoader instanceof PluginClassLoader) {
-			final var plugin = ((PluginClassLoader) classLoader).getPlugin();
+		return INTERNAL_generateLogger(clazz.getClassLoader(), clazz);
+	}
+
+	/**
+	 * Create a logger based on a given class and a deliberately specified plugin. Use this in shared classes and such
+	 * which are used by other plugins. For example, {@link vg.civcraft.mc.civmodcore.command.AikarCommandManager} is
+	 * used by multiple plugins, thus it would be disingenuous for it to log as if the messages originate from
+	 * CivModCore instead of the plugin that instantiated the class.
+	 *
+	 * @param pluginClass The instance-originating plugin class.
+	 * @param targetClass The target class to log about.
+	 * @return Returns a new civ logger.
+	 */
+	public static CivLogger getLogger(@Nonnull final Class<? extends Plugin> pluginClass,
+									  @Nonnull final Class<?> targetClass) {
+		return INTERNAL_generateLogger(pluginClass.getClassLoader(), targetClass);
+	}
+
+	private static CivLogger INTERNAL_generateLogger(@Nonnull final ClassLoader loader,
+													 @Nonnull final Class<?> clazz) {
+		if (loader instanceof PluginClassLoader pluginClassLoader) {
+			final var plugin = pluginClassLoader.getPlugin();
 			if (plugin != null) {
 				return new CivLogger(plugin.getLogger(), clazz.getSimpleName());
 			}
 			// Plugin has been constructed but not initialised yet
 			final var descriptionField = FieldUtils.getDeclaredField(PluginClassLoader.class, "description", true);
 			try {
-				final var description = (PluginDescriptionFile) descriptionField.get(classLoader);
+				final var description = (PluginDescriptionFile) descriptionField.get(loader);
 				final var logger = PaperPluginLogger.getLogger(description);
 				return new CivLogger(logger, clazz.getSimpleName());
 			}
