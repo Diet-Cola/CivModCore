ae21bdf80abc3fe723793bf004c24702cbc32f82
Alexander <protonull@protonmail.com>
2021-06-27 17:53:48 +0100

    Replaced EntityUtils.isPlayer() with instanceof

--- src/main/java/vg/civcraft/mc/civmodcore/entities/EntityUtils.java
+++ src/main/java/vg/civcraft/mc/civmodcore/entities/EntityUtils.java
@@ -2,9 +2,7 @@ package vg.civcraft.mc.civmodcore.entities;
 
 import com.google.common.base.Strings;
 import lombok.experimental.UtilityClass;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
 
 /**
  * Class of static APIs for Entities.
@@ -35,25 +33,6 @@ public final class EntityUtils {
 		}
 		catch (final Exception ignored) { }
 		return null;
-	}
-
-	/**
-	 * Checks whether an entity is a player.
-	 *
-	 * @param entity The entity to test.
-	 * @return Returns true if the entity is a player.
-	 */
-	public static boolean isPlayer(final Entity entity) {
-		if (entity == null) {
-			return false;
-		}
-		if (entity.getType() != EntityType.PLAYER) {
-			return false;
-		}
-		if (!(entity instanceof Player)) {
-			return false;
-		}
-		return true;
 	}
 
 }
--- src/main/java/vg/civcraft/mc/civmodcore/inventory/gui/ClickableInventoryListener.java
+++ src/main/java/vg/civcraft/mc/civmodcore/inventory/gui/ClickableInventoryListener.java
@@ -6,7 +6,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import vg.civcraft.mc.civmodcore.entities.EntityUtils;
 
 /**
  * The listener which makes ClickableInventories work. To use this either register it as a listener in your plugin or
@@ -18,23 +17,20 @@ public class ClickableInventoryListener implements Listener {
 
 	@EventHandler
 	public void inventoryClick(InventoryClickEvent event) {
-		if (!EntityUtils.isPlayer(event.getWhoClicked())) {
-			return;
-		}
-		Player player = (Player) event.getWhoClicked();
-		ClickableInventory inventory = ClickableInventory.getOpenInventory(player);
-		if (inventory != null) {
-			event.setCancelled(true); // always cancel first to prevent dupes
-			inventory.itemClick(player, event.getRawSlot(), event.getClick());
+		if (event.getWhoClicked() instanceof Player player) {
+			ClickableInventory inventory = ClickableInventory.getOpenInventory(player);
+			if (inventory != null) {
+				event.setCancelled(true); // always cancel first to prevent dupes
+				inventory.itemClick(player, event.getRawSlot(), event.getClick());
+			}
 		}
 	}
 
 	@EventHandler
 	public void inventoryClose(InventoryCloseEvent event) {
-		if (!EntityUtils.isPlayer(event.getPlayer())) { // Despite the name, it's not necessarily a player
-			return;
+		if (event.getPlayer() instanceof Player player) { // Despite the name, it's not necessarily a player
+			ClickableInventory.inventoryWasClosed(player);
 		}
-		ClickableInventory.inventoryWasClosed((Player) event.getPlayer());
 	}
 
 	@EventHandler
